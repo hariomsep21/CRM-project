54b18d34c2424dcc1f90b92d09afe0d1f2345923
hariom <hariom@gmail.com>
2024-07-10 16:25:46 +0530

    Customer first page completed

--- CRM-Project/src/App.jsx
+++ CRM-Project/src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import Customer from "./components/Customers_Comp/Customer";
 import LoginPage from "./components/Login_component/LoginPage";
 
 function App() {
   return (
     <>
       <div className="main">
-        <LoginPage />
+        <Customer />
       </div>
     </>
   );
---
+++ CRM-Project/src/components/Customers_Comp/CustomerGrid.jsx
@@ -0,0 +1,111 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import style from "./CustomerGrid.module.css";
+import { CiLocationOn } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
+import { RiMessage2Fill } from "react-icons/ri";
+import { IoMdMail } from "react-icons/io";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { HiDownload } from "react-icons/hi";
+import { TfiPencil } from "react-icons/tfi";
+
+const CustomerGrid = ({ customers }) => {
+  return (
+    <>
+      {customers.map((customer) => (
+        <div key={customer.id} className={`row mt-5 ${style.cust_rowThree}`}>
+          <div className="col-sm-12 col-md-4 col-lg-4">
+            <div className="col">
+              <button
+                className={`btn ${
+                  customer.property === "Buyer/Seller"
+                    ? style.property_btn_buyer
+                    : style.property_btn_rental
+                }`}
+              >
+                {customer.property}
+              </button>
+            </div>
+            <div className="col">
+              <div className={style.cust_name}>
+                {customer.name}
+                <p>{customer.status}</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className={style.cust_address}>
+                <CiLocationOn />
+                <h6>{customer.address}</h6>
+              </div>
+            </div>
+            <div className={`col mt-2 ${style.cust_icons} `}>
+              <div className="col-2">
+                <FaPhoneAlt />
+              </div>
+              <div className="col-2">
+                <RiMessage2Fill />
+              </div>
+              <div className="col-2">
+                <IoMdMail />
+              </div>
+              <div className="col-2">
+                <IoLogoWhatsapp />
+              </div>
+              <div className="col-2">
+                <HiDownload />
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-4 col-lg-4 mt-3">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="row d-flex">
+                  <div className={`col-8 ${style.remark_head}`}>Need</div>
+                  <div className={`col-4 ${style.remark_icon}`}>
+                    <div>
+                      <TfiPencil />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={`col-lg-12  mt-1 ${style.remark_heading}`}>
+                <p>{customer.need}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-4 col-lg-4 mt-3">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="row d-flex">
+                  <div className={`col-8 ${style.remark_head}`}>Remarks</div>
+                  <div className={`col-4 ${style.remark_icon}`}>
+                    <div>
+                      <TfiPencil />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={`col-lg-12  mt-1 ${style.remark_heading}`}>
+                <p> {customer.remarks}</p>
+              </div>
+              <div className="col-sm">
+                <div className={`row d-flex ${style.remark_btn}`}>
+                  <div className="col-4">
+                    <button className="btn">View Past Deals</button>
+                  </div>
+                  <div className="col-4">
+                    <button className="btn">References</button>
+                  </div>
+                  <div className="col-4">
+                    <button className="btn">Edit Customer</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default CustomerGrid;
